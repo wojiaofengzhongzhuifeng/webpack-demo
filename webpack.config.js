50b67336af9b3ecdcecd72510db3f3971d357f11
饶家俊 <471412450@qq.com>
2021-03-03 14:45:35 +0800

    格式化

--- webpack.config.js
+++ webpack.config.js
@@ -4,17 +4,18 @@ module.exports = {
   entry:path.resolve(__dirname, './src/index.js'),
   // 1️⃣ webpack 默认认识 .js 文件, 其他文件需要使用 module 进行配置,使用相应的 loader 处理
   module: {
-    rules: [{
-      test: /\.(png|jpg|gif)$/,
-      use: {
-        // 2️⃣ 使用 url-loader 如果文件大于 2kb 的话,使用 file-loader 小于 2kb 的话,使用 url-loader
-        loader: 'url-loader',
-        options: {
-          limit: 2048,
-        }
+    rules: [
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: {
+          // 2️⃣ 使用 url-loader 如果文件大于 2kb 的话,使用 file-loader 小于 2kb 的话,使用 url-loader
+          loader: 'url-loader',
+          options: {
+            limit: 2048,
+          }
+        },
       },
-
-    }]
+    ]
   },
   output: {
     filename: 'test.js',
