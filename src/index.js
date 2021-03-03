1897e825ce4e4579c3c9b0da2c4e8dbdc20c56b6
饶家俊 <471412450@qq.com>
2021-03-03 14:01:57 +0800

    使用 file-loader 打包图片

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import avatar from './未命名.png';
+
+console.log(avatar);
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,16 @@
 const path = require('path')
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry:path.resolve(__dirname, './src/index.js'),
+  // 1️⃣ webpack 默认认识 .js 文件, 其他文件需要使用 module 进行配置,使用相应的 loader 处理
+  module: {
+    rules: [{
+      test: /\.png$/,
+      use: {
+        loader: 'file-loader'
+      }
+    }]
+  },
   output: {
     filename: 'test.js',
     path: path.resolve(__dirname, './dist')
