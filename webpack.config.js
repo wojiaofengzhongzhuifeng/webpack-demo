a749e6a34ac46442e4d44949e2522fcaa2ea3ef9
饶家俊 <471412450@qq.com>
2021-03-04 15:25:58 +0800

    切换为业务配置

--- webpack.config.js
+++ webpack.config.js
@@ -80,29 +80,29 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
-          // options: {
-          //   presets: [['@babel/preset-env', {
-          //     targets: {
-          //       "chrome": "58",
-          //       "ie": "11"
-          //     },
-          //     useBuiltIns: "usage"
-          //   }]],
-          // }
           options: {
-            "plugins": [
-              [
-                "@babel/plugin-transform-runtime",
-                {
-                  "absoluteRuntime": false,
-                  "corejs": 2,
-                  "helpers": true,
-                  "regenerator": true,
-                  "version": "7.0.0-beta.0"
-                }
-              ]
-            ]
-          }
+            presets: [['@babel/preset-env', {
+              targets: {
+                "chrome": "58",
+                "ie": "11"
+              },
+              useBuiltIns: "usage"
+            }]],
+          },
+          // options: {
+          //   "plugins": [
+          //     [
+          //       "@babel/plugin-transform-runtime",
+          //       {
+          //         "absoluteRuntime": false,
+          //         "corejs": 2,
+          //         "helpers": true,
+          //         "regenerator": true,
+          //         "version": "7.0.0-beta.0"
+          //       }
+          //     ]
+          //   ]
+          // }
         }
       }
     ]
